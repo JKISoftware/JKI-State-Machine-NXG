db7fa5a40898d52227fda71162397d23b39ac75e
Jim Kring <jim.kring@gmail.com>
2017-05-03 16:26:30 -0700

    - Added batch file to rebuild repo, launch NIPM ,and then launch NXG for easy iterative testing - Added description to template project - Added some docs and helps to template VI

---
+++ repo/1.0.0.0/Update Repo + Launch NIPM + Launch NXG.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\National Instruments\NI Package Manager\nipkg.exe" feed-create .\
+"C:\Program Files\National Instruments\NI Package Manager\NIPackageManager.exe"
+"C:\Program Files\National Instruments\LabVIEW NXG\LabVIEW NXG.exe"
